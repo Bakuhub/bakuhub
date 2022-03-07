fa598bea49b584662aa80efb76718a5d37011869
baku <so2die1248@gmail.com>
2022-03-07 15:24:07 +0000

    feat: 🎸 add material ui and clean up code

---
+++ src/components/MainPage/index.tsx
@@ -0,0 +1,9 @@
+import {Grid, Typography} from "@mui/material";
+
+export const MainPage = () => {
+    return <Grid>
+        <Typography variant={"h6"}>
+            Bakuhub
+        </Typography>
+    </Grid>
+}
