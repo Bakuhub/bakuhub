182aa5ab779740f09394a63049507b49d59e4f6b
baku <so2die1248@gmail.com>
2022-03-07 15:44:23 +0000

    feat: 🎸 init redux and update eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import type {NextPage} from 'next'
+import type {NextPage} from "next";
 import {MainPage} from "../src/components/MainPage";
+import {useSelector} from "react-redux";
 
 const Home: NextPage = () => {
+    const number = useSelector(state => state);
+    console.info(number);
     return (
         <MainPage/>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
