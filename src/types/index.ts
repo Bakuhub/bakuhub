5c7ed36c21ab432a77c32111f8b778b80248f19e
baku <so2die1248@gmail.com>
2022-03-08 21:58:13 +0000

    feat: 🎸 add initial types

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+export interface Premise {
+    id: number;
+    status: string;
+    tags: string[];
+    title: string;
+    date: Date;
+    history: string[];
+}
+
+export interface Vision {
+    id: string;
+    title: string;
+    description: string;
+    reference: Reference;
+    author: User;
+}
+
+interface User {
+    username: string;
+    id: string;
+}
+
+interface UserDetail {
+    id: string;
+    userId: string;
+    credibility: number;
+    numberOfPost: number;
+    area: string;
+}
+
+interface Reference {
+    type: "image" | "video" | "string";
+    value: string;
+}
