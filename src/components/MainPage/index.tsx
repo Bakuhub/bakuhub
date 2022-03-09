438effad19ea8a7f0878688b5efae4a8f2a8bb35
baku <so2die1248@gmail.com>
2022-03-09 08:25:57 +0000

    feat: 🎸 add sample search bar, premise and icon

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,9 +1,25 @@
-import {Grid, Typography} from "@mui/material";
+import {Grid, TextField, Typography} from "@mui/material";
+import PremiseOverview from "../Premise";
 
 export const MainPage = () => {
-    return <Grid>
-        <Typography variant={"h6"}>
-            Bakuhub
-        </Typography>
+
+    return <Grid container spacing={1} justifyContent={"center"}>
+        <Grid item container justifyContent={"center"} xs={12}>
+            <Typography variant={"h6"}>
+                Bakuhub
+            </Typography>
+        </Grid>
+        <Grid item container justifyContent={"center"} xs={12}>
+            <TextField id="outlined-basic" label="Search" variant="outlined"/>
+        </Grid>
+        <Grid>
+
+        </Grid>
+        {
+            new Array(1).fill(1).map(
+                    g => <Grid key={g} item><PremiseOverview/></Grid>
+            )
+
+        }
     </Grid>;
 };
