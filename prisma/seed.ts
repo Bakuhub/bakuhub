c351c5f8461c5353fe31ac6e1055b89fe004097c
baku <so2die1248@gmail.com>
2022-03-13 00:49:21 +0000

    feat: 🎸 update prisma and npm commands

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,6 +4,7 @@ const prisma = new PrismaClient({});
 
 // A `main` function so that you can use async/await
 async function seed() {
+
     // Create user, posts, and categories
     // const user = await prisma.premise.create({
     //     data: {
@@ -12,7 +13,32 @@ async function seed() {
     //
     //     },
     // });
-
+    // await prisma.post.create({
+    //     data: {
+    //         title: "Hello",
+    //         categories: {
+    //             create: [
+    //                 {
+    //                     category: {
+    //                         create: {
+    //                             name: "category-1",
+    //                         },
+    //                     },
+    //                 },
+    //                 {category: {connect: {id: 10}}},
+    //             ],
+    //         },
+    //     },
+    // });
+    const premise = await prisma.premise.create({
+        data: {
+            title: "More than half of UK voters still think Boris Johnson should resign",
+            status: "UNVERIFIED",
+            tags: ["UK"],
+            authorId: "dde6d01b-cc10-46cf-9c4a-e7ca7f3268e3",
+            activityDate: new Date()
+        }
+    });
     // Return user, and posts, and categories
     const returnUser = await prisma.user.create({
         data: {
