e29d0c139789be31da02da6eac6d72428b184ea0
baku <bakuhub27@gmail.com>
2022-04-17 21:44:54 +0100

    fix: 🐛 emit schema file only in dev

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -4,7 +4,7 @@ import {resolvers} from "../prisma/generated/type-graphql";
 export async function createSchema() {
     return await buildSchema({
                                  resolvers,
-                                 emitSchemaFile: process.env.NODE_ENV !== "development",
+                                 emitSchemaFile: process.env.NODE_ENV === "development",
                                  validate: false,
                              });
 }
