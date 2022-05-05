3d7e2942cd5636742116a9f1c656a26f4d5a997d
baku <bakuhub27@gmail.com>
2022-05-06 00:40:33 +0100

    fix: 🐛 use ssrapollo and ssr for premise

--- pages/premise/[id].tsx
+++ pages/premise/[id].tsx
@@ -1,5 +1,8 @@
-import {PremiseDetailContainer} from "../../src/components/Premise/PremiseDetail";
+import {PremiseDetail} from "../../src/components/Premise/PremiseDetail";
 import {GetServerSideProps} from "next";
+import {getSsrApollo} from "../../src/lib/apollo";
+import {premiseQuery} from "../../src/gql/query/premiseQuery";
+import {getPremiseDetailQueryVariable} from "../../src/gql/utils/getPremiseDetailQueryVariable";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     context.res.setHeader(
@@ -7,25 +10,18 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
             "public, s-maxage=10, stale-while-revalidate=59"
     );
     const premiseId = context.query.id;
-    // const apollo = getSsrApollo(context.req);
-    // console.time("apollo");
-    // const {data} = await apollo.query({
-    //                                       query: premiseQuery,
-    //                                       ...getPremiseDetailQueryVariable(premiseId as string)
-    //
-    //                                   });
-    // console.timeEnd("apollo");
-    // console.time("prisma");
-    // const g = await prisma.premise.findFirst({
-    //                                              where: {
-    //                                                  id: premiseId as string
-    //                                              }
-    //                                          });
-    // console.timeEnd("prisma");
+    const apollo = getSsrApollo(context.req);
+    console.time("apollo");
+    const {data} = await apollo.query({
+                                          query: premiseQuery,
+                                          ...getPremiseDetailQueryVariable(premiseId as string)
+
+                                      });
+    console.timeEnd("apollo");
     return {
         props: {
-            premiseId: premiseId as string,
-            // premise: data.premise
+            //premiseId: premiseId as string,
+            premise: data.premise
         }
     };
 };
@@ -37,4 +33,4 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 //     if (res.data) return <PremiseDetail premise={res.data.premise}/>;
 // };
 
-export default PremiseDetailContainer;
+export default PremiseDetail;
